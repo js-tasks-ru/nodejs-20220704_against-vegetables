0bbb33dede0beb42fa53cad56da8c6bd050cd9a3
against-v <against.vegetables@gmail.com>
2022-07-28 00:04:39 +0300

    add solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,22 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.items = [];
+    this.currentString = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.currentString += chunk.toString();
+    this.items = this.currentString.split(os.EOL);
+    this.currentString = this.items.pop();
+    this.items.forEach((item) => {
+      this.push(item);
+    });
+    callback(null);
   }
 
   _flush(callback) {
+    callback(null, this.currentString);
   }
 }
 
