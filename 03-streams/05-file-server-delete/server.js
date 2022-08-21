7e03264d506f1e60e5c5b3555f24bbb7f301723c
against-v <against.vegetables@gmail.com>
2022-08-21 22:02:53 +0300

    add solution for 3-module 5-task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,10 +1,11 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
@@ -12,6 +13,22 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Bad request');
+        break;
+      }
+
+      try {
+        fs.rmSync(filepath);
+        res.statusCode = 200;
+        res.end();
+      } catch (error) {
+        if (error && error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        }
+      }
 
       break;
 
